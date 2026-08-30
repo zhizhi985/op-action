c28ee10b9d39e4fcb7c830dcf37f75aa1eeb096e
zhizhi985 <98744474+zhizhi985@users.noreply.github.com>
2026-08-31 00:35:46 +0800

    Update diy-part1.sh

--- diy-part1.sh
+++ diy-part1.sh
@@ -12,7 +12,12 @@
 
 # Uncomment a feed source
 #sed -i 's/^#\(.*helloworld\)/\1/' feeds.conf.default
-
+# 克隆 OpenClash 源码[reference:5]
+git clone https://github.com/vernesong/OpenClash.git package/OpenClash
+# 克隆 luci-app-mosdns 源码[reference:6]
+git clone https://github.com/sbwml/luci-app-mosdns.git package/mosdns
+# 克隆 ddns-go 源码[reference:7]
+git clone https://github.com/sirpdboy/luci-app-ddns-go.git package/ddns-go
 # Add a feed source
-echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
+#echo 'src-git helloworld https://github.com/fw876/helloworld' >>feeds.conf.default
 #echo 'src-git passwall https://github.com/xiaorouji/openwrt-passwall' >>feeds.conf.default
